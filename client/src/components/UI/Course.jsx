93f5e6d22f6ba8e6883f646007007cd7cc7fb969
tianna-spears <140460149+tianna-spears@users.noreply.github.com>
2025-06-23 19:47:26 +0100

    quick edits for MUI and added a dropdown menu to Course component in dashboard

--- client/src/components/UI/Course.jsx
+++ client/src/components/UI/Course.jsx
@@ -1,15 +1,52 @@
 import React from "react";
+import { Select, Button, Menu, MenuItem, Typography, FormControl, InputLabel } from '@mui/material'
+import PopupState, { bindTrigger, bindMenu } from 'material-ui-popup-state';
+import { useState } from "react";
 
 const Course = () => {
-    return (
-        <div>
-          <h1> I am currently studying *course*
-            dropdown scroll - Intro to Programming, React, Node /Express, Python
-            save to user function 
+    const [ selectCourse, setSelectCourse ] =useState('')
 
-            CRUD </h1>  
-        </div>
-    )
-}
+    const handleSelect = (course, popupState) => {
+        setSelectCourse(course)
+        popupState.close()
+    }
+
+  return (
+    <div>
+    <PopupState variant="popover" popupId="demo-popup-menu">
+      {(popupState) => (
+        <React.Fragment>
+          <Button variant="contained" {...bindTrigger(popupState)}>
+             {selectCourse ? `Course: ${selectCourse}` : "Select a Course"}
+          </Button>
+           <Menu {...bindMenu(popupState)}>
+              {[
+                "Introduction to Programming",
+                "React.js",
+                "Node/Express",
+                "Ruby on Rails",
+                "Python",
+                "Advanced Practicum",
+              ].map((course) => (
+                <MenuItem
+                  key={course}
+                  onClick={() => handleSelect(course, popupState)}
+                >
+                  {course}
+                </MenuItem>
+              ))}
+            </Menu>
+          </React.Fragment>
+        )}
+      </PopupState>
+
+      {selectCourse && (
+        <Typography variant="body1" style={{ marginTop: "1rem" }}>
+          You selected: <strong>{selectCourse}</strong>
+        </Typography>
+      )}
+    </div>
+  );
+};
 
 export default Course;
