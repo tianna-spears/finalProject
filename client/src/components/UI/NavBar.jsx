859a3b4abcf225647b063363b3a1502e84751345
tianna-spears <140460149+tianna-spears@users.noreply.github.com>
2025-06-23 09:37:44 +0100

    changed to MaterialUI for design, code cleanup on client side.

--- client/src/components/UI/NavBar.jsx
+++ client/src/components/UI/NavBar.jsx
@@ -1,21 +1,21 @@
 import React from "react";
 import { ImArrowUpRight2 } from "react-icons/im";
 
-
 const NavBar = () => {
   return (
-<nav className="fixed w-full bg-white text-purple p-4 top-0 left-0 shadow-md z-50">
-      <div className="max-w-7xl mx-auto flex justify-between">
-        <div className="text-xl font-bold text-black">
-            
-            <h3> Apprenti <ImArrowUpRight2 /> </h3> 
-            </div>
-<div className="flex flex-col space-y-2">
-  <a href="#home" className="hover:underline">Home</a>
-  <a href="#register" className="hover:underline">Register</a>
-  <a href="#login" className="hover:underline">Login</a>
-</div>
-
+    <nav>
+      <div>
+        <div>
+          <h3>
+            {" "}
+            Apprenti <ImArrowUpRight2 />{" "}
+          </h3>
+        </div>
+        <div>
+          <a href="">Home</a>
+          <a href="">Register</a>
+          <a href="">Login</a>
+        </div>
       </div>
     </nav>
   );
--- client/src/components/pages/Register.jsx
+++ client/src/components/pages/Register.jsx
@@ -1,48 +1,30 @@
 import React from "react";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
 
 const Register = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100 p-8 m-8">
-      <form className="border-4 border-red-600 rounded-xl w-full max-w-md p-8 m-8">
-        <h3 className="text-2xl font-bold text-center text-gray-800">
-          {" "}
-          Register:{" "}
-        </h3>
+    <div>
+      <form>
+        <Card variant="outlined">
+          <CardHeader>Register </CardHeader>
+          <div>
+            <TextField label="FirstName" color="secondary" focused />
 
-        <div>
-          <label className="block mb-1 text-gray-700"> First Name: </label>
-          <input
-            className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
-            type="text"
-            name="firstname"
-            placeholder="Enter first name here"
-          />
-        </div>
+            <TextField label="Last Name" color="secondary" focused />
 
-        <div>
-          <label className="block mb-1 text-gray-700"> Last Name: </label>
-          <input
-            className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
-            type="text"
-            name="lastname"
-            placeholder="Enter last name here"
-          />
-        </div>
+            <TextField label="Email" color="secondary" focused />
+          </div>
 
-        <div>
-          <label className="block mb-1 text-gray-700"> Email: </label>
-          <input
-            className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
-            type="email"
-            name="email"
-            placeholder="Enter email here"
-          />
-        </div>
-
-        <button className="block mb-1 bg-pink-200" type="submit">
-          {" "}
-          Submit{" "}
-        </button>
+          <Stack spacing={2} direction="row">
+            <Button type="submit" variant="outlined">
+              Register
+            </Button>
+          </Stack>
+        </Card>
       </form>
     </div>
   );
