b98dabf377b0a8fd22bc1f8755bbd17243ea71a2
tianna-spears <140460149+tianna-spears@users.noreply.github.com>
2025-06-24 01:02:46 +0100

    updated registerController and registerRoute to affect students across the app. Models work just fine.

--- server/controllers/register.js
+++ server/controllers/register.js
@@ -1,14 +1,15 @@
-const registerUser = async(req, res) => {
-    res.send('Register user')
-}
+const User = require("../models/User");
 
-// const registerUser  = async (req, res) => {
-//     const { firstName, lastName, email, password } = req.body;
+const registerUser = async (req, res) => {
+  const { firstName, lastName, email, password } = req.body;
 
-//     if(!firstName && !lastName && !email && !password) {
-//         return res.status(500).send('User not found. Please try again')
-//     }
-//     res.status(200).send('User successfully created!')
-// }
+  if (!firstName || !lastName || !email || !password) {
+    return res.status(400).json({ error: "Please fill in all fields."});
+  }
+  const newUser = new User({ firstName, lastName, email, password })
+  await newUser.save();
 
-module.exports = registerUser
+  res.status(201).json({message: "User successfully created!", user: newUser});
+};
+
+module.exports = registerUser;
