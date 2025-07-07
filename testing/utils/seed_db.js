50a137f99bc467f4f3981f6f6a22f9b7c19d66d2
tianna-spears <140460149+tianna-spears@users.noreply.github.com>
2025-07-07 23:14:30 +0100

    debugging tests updates

--- testing/utils/seed_db.js
+++ testing/utils/seed_db.js
@@ -6,7 +6,6 @@ const Course = require("../../server/models/Course");
 const faker = require("@faker-js/faker").fakerEN_US;
 const FactoryBot = require("factory-bot");
 
-// link to server .env file through setup.js helper
 const MONGODB_URI_TEST = require("../setup");
 
 const testUserPassword = faker.internet.password();
@@ -15,21 +14,21 @@ const factoryAdapter = new FactoryBot.MongooseAdapter();
 factory.setAdapter(factoryAdapter);
 factory.define("course", Course, {
   courseName: () => faker.lorem.words(4),
-  courseDates: () => {
-    const start = faker.date.future();
-    const end = faker.date.between(
-      start,
-      new Date(start.getTime())
-    );
-    const formatDate = (date) => {
-      const mm = String(date.getMonth() + 1).padStart(2, "0");
-      const dd = String(date.getDate()).padStart(2, "0");
-      const yyyy = date.getFullYear();
-      return `${mm}/${dd}/${yyyy}`;
-    };
-    return `${formatDate(start)} - ${formatDate(end)}`;
-  },
-});
+courseDates: () => {
+  const start = faker.date.future();
+  const end = faker.date.between({
+    from: start,
+    to: new Date(start.getTime() + 7 * 24 * 60 * 60 * 1000),
+  });
+  const formatDate = (date) => {
+    const mm = String(date.getMonth() + 1).padStart(2, "0");
+    const dd = String(date.getDate()).padStart(2, "0");
+    const yyyy = date.getFullYear();
+    return `${mm}/${dd}/${yyyy}`;
+  };
+  return `${formatDate(start)} - ${formatDate(end)}`;
+}
+})
 factory.define("user", User, {
   firstName: () => faker.person.firstName(),
   lastName: () => faker.person.lastName(),
@@ -42,8 +41,9 @@ const seed_db = async () => {
   let testUser = null;
   try {
     // let MONGODB_URI_TEST = process.env.MONGODB_URI_TEST;
-    await Course.deleteMany({}); // deletes all course records
-    await User.deleteMany({}); // and deleted all users
+    await Course.deleteMany({});
+    await User.deleteMany({}); 
+    await Course.create({ courseName: 'Node Express', courseDates: [] });
     testUser = await factory.create("user", { password: testUserPassword });
     await factory.createMany("course", 10, { createdBy: testUser._id }); // put 10 course entries in the database.
   } catch (e) {
